8b1b469d722243e5a5059454546e0acd9adf51df
Dorian-rx <rouxdorian@eisti.eu>
2021-04-21 15:19:53 +0200

    Test

---
+++ Ressources/Ressources_perTeacher.py
@@ -0,0 +1,20 @@
+###################################
+    #Ressources par Enseignant#
+###################################
+
+# Import Packages
+import random
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+# Définir les paramètres pandas
+#pd.set_option("display.max_rows", 20)
+#pd.set_option("display.max_columns", 10)
+
+
+# Definir un seed pour garder les même valeurs aléatoirement définies.
+random.seed( 1965 )
+
