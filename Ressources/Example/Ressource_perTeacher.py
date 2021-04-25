706a491df81d3b6d986aa86a44203b7dfd7b25f8
Dorian-rx <rouxdorian@eisti.eu>
2021-04-25 12:16:37 +0200

    ReWorked - 25/04/2021 (Only changes required on the read.md)

--- Ressources/Example/Ressource_perTeacher.py
+++ Ressources/Example/Ressource_perTeacher.py
@@ -3,24 +3,27 @@
 ###################################
 
 # Import Packages
+#Basic packages
 import random
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+#######################################################
 
 # Définir les paramètres pandas
 pd.set_option("display.min_rows", 100)
 #pd.set_option("display.max_columns", 10)
-
 
 # Definir un seed pour garder les même valeurs aléatoirement définies.
 random.seed( 2000 )
 
 #Import CSV
 Course_PerYear = pd.read_csv('Ressources\Example\Data\Ressources_perTeacher.csv')
+print(Course_PerYear.head(10))
 
-video = ['Course_ID', 'Year', 'Duration_Video', 'Number_Video', 'Number_Video_1H', 'Number_Video_2H', 'Number_video_3H', 'Number_Video_4H']
-Course_PerYear_Video = Course_PerYear[video]
+for i in Course_PerYear['Year']:
+    Course_PerYear['Courses_perYear'] == len(Course_PerYear['Course_Duration'][Course_PerYear['Year'] == year])
 
-print(Course_PerYear_Video)
+print(Course_perYear)
